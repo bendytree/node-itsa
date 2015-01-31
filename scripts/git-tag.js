4980e0fe37bcc32d450157ef611b30f3072c4d4b
Josh Wright <help@bendytree.com>
2015-01-30 22:15:53 -0600

    added v prefix to git tag

--- scripts/git-tag.js
+++ scripts/git-tag.js
@@ -2,7 +2,7 @@
 require('shelljs/global');
 
 var version = require('../package.json').version;
-if (exec('git tag '+version).code !== 0) {
+if (exec('git tag v'+version).code !== 0) {
   echo('Error: Git tag failed');
   exit(1);
 }
