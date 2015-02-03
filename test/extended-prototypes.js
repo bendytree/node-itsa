c4b47988cd70598ef8496b51117ef5aca97fe782
Josh Wright <help@bendytree.com>
2015-02-02 21:54:40 -0600

    fixed issues where extended native objects broke validation

--- test/extended-prototypes.js
+++ test/extended-prototypes.js
@@ -10,10 +10,29 @@ describe('extended prototypes', function(){
     delete Array.prototype.dumb;
   });
 
+  it('of array - does not break validation of nested array', function(){
+    Array.prototype.dumb = function(){};
+    assert.equal(itsa.arrayOf(itsa.array()).validate([[]]).valid, true);
+    delete Array.prototype.dumb;
+  });
+
   it('of object - does not break validation', function(){
     Object.prototype.dumb = function(){};
     assert.equal(itsa.object({}).validate({}).valid, true);
     delete Object.prototype.dumb;
+  });
+
+  it('of object - does not break validation on nested object', function(){
+    Object.prototype.dumb = function(){};
+    var result = itsa.object({a:itsa.object()}).validate({a:{}});
+    assert.equal(result.valid, true, "Should have been valid. " + result.describe());
+    delete Object.prototype.dumb;
+  });
+
+  it('of array - does not break `any` validation', function(){
+    Array.prototype.dumb = "dumb";
+    assert.equal(itsa.any(itsa.object()).validate("dumb").valid, false);
+    delete Array.prototype.dumb;
   });
 
 });
