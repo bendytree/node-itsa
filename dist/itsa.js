7c944d50e45b7ea13388c2d041d6b6dbbd48ee91
Josh Wright <help@bendytree.com>
2015-02-25 12:26:50 -0600

    Bundling and version bump

--- dist/itsa.js
+++ dist/itsa.js
@@ -1,7 +1,7 @@
 /*! 
   * @license 
-  * itsa 1.2.17 <https://github.com/bendytree/node-itsa> 
-  * Copyright 2/23/2015 Josh Wright <http://www.joshwright.com> 
+  * itsa 1.2.18 <https://github.com/bendytree/node-itsa> 
+  * Copyright 2/25/2015 Josh Wright <http://www.joshwright.com> 
   * MIT LICENSE <https://github.com/bendytree/node-itsa/blob/master/LICENSE> 
   */ 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.itsa=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
