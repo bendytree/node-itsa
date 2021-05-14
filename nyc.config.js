101f1e07c505b4fbf47795057c5f786d80024b12
Josh Wright <help@bendytree.com>
2021-05-14 16:51:38 -0500

    added readme

--- nyc.config.js
+++ nyc.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   reporter: ['text', 'html'],
-  'skip-full': true,
+  'skip-full': false,
   'temp-dir': '/tmp/nyc',
   'report-dir': './.coverage',
   'exclude': [ '**/*.spec.ts' ],
