5ff7ad2fea559599f7454ddd87087b7266e7140e
Josh Wright <352285+bendytree@users.noreply.github.com>
2023-01-18 17:23:10 -0600

    added function cloning

---
+++ dist/types/partialed.d.ts
@@ -0,0 +1,9 @@
+import { Itsa } from "./itsa";
+export declare class ItsaPartialed {
+    partialed(this: Itsa): Itsa;
+}
+declare module './itsa' {
+    interface Itsa extends ItsaPartialed {
+    }
+}
+//# sourceMappingURL=partialed.d.ts.map
